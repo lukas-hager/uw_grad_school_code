ad28fab8883959fba8a9ff21778e0b95faef3a02
Lukas Hager <lghhager@uw.edu>
2023-04-08 22:18:47 -0700

    few cosmetic and commenting edits

--- ECON-527/ps1.jl
+++ ECON-527/ps1.jl
@@ -2,35 +2,19 @@ using QuantEcon
 using StatsBase
 using Statistics
 using BasicInterpolators
+using Random
 using Plots
 using Printf
 
-################################################################################
-# Discretizing an AR(1)
-################################################################################
+# set the seed
+Random.seed!(42069)
 
-# define the parameters of the AR process
-rho = .99
-sigma = .2
-mu = 0
+# set the output path
+path = "/Users/hlukas/Google Drive/Grad School/2022-2023/Spring/ECON 527/Assignments/Assignment_1/"
 
-# define the tauchen parameters
-gridpoints = 9
-n_std = 3
+# functions that are used throughtout the script (mostly formatting results)
 
-# define the simulation parameters
-
-n_sims = 1000
-n_periods = 1000
-
-# get the approximations
-tauchen_appx_3 = QuantEcon.tauchen(gridpoints,rho,sigma,mu,n_std)
-rouwenhorst_appx = QuantEcon.rouwenhorst(gridpoints,rho,sigma,mu)
-
-# add the extra approximation
-tauchen_appx_2_5 = QuantEcon.tauchen(gridpoints,rho,sigma,mu,2.5)
-
-# write a function to provide the relevant statistics
+# compare the moments of the approximation to the theoretical moments
 function appx_stats(appx)
 
     # first calculate the invariant distribution for seeding the simulations
@@ -64,6 +48,54 @@ function appx_stats(appx)
 
 end
 
+# the CRRA utility function
+function u(c)
+    c.^(1-gamma) / (1-gamma)
+end
+
+# calculating the percentage of an array of true/false that is true
+function perc_true(arr)
+    sum(arr) * 100. / length(arr)
+end
+
+# test the relevant properties for an interpolated function
+function check_properties(x, y)
+    y_diff = y[2:end] - y[1:(end-1)]
+    x_diff = x[2:end] - x[1:(end-1)]
+    diff_ratio = y_diff ./ x_diff
+
+    @printf "Monotonicity: %.1f%%\n" perc_true(y_diff .> 0)
+    @printf "Concavity: %.1f%%\n" perc_true(diff_ratio[1:(end-1)] .> diff_ratio[2:end])
+    @printf "Negativity: %.1f%%\n" perc_true(y .< 0)
+
+end
+
+################################################################################
+# Discretizing an AR(1)
+################################################################################
+
+# define the parameters of the AR process
+rho = .99
+sigma = .2
+mu = 0
+
+# define the tauchen parameters
+gridpoints = 9
+n_std = 3
+
+# define the simulation parameters
+
+n_sims = 1000
+n_periods = 1000
+
+# get the approximations
+tauchen_appx_3 = QuantEcon.tauchen(gridpoints,rho,sigma,mu,n_std)
+rouwenhorst_appx = QuantEcon.rouwenhorst(gridpoints,rho,sigma,mu)
+
+# add the extra approximation
+tauchen_appx_2_5 = QuantEcon.tauchen(gridpoints,rho,sigma,mu,2.5)
+
+# calculate and print the results
 println("2. Tauchen Approximation (3 SD)")
 appx_stats(tauchen_appx_3)
 
@@ -77,19 +109,17 @@ appx_stats(tauchen_appx_2_5)
 # Interpolation
 ################################################################################ 
 
+# define the parameters
 gamma = 4.
 a = .1
 b = 30.
 c = 3.
 
-function u(c)
-    c.^(1-gamma) / (1-gamma)
-end
-
+# get a grid for c, use it and u(c) to interpolate
 c_i = range(a, b, length=100)
-u_c_i = u(c_i)
-itp = CubicInterpolator(c_i, u_c_i)
+itp = CubicInterpolator(c_i, u(c_i))
 
+# get a finer grid for testing the interpolation
 x_j = range(a, b, length=1000)
 
 println("2 (a)")
@@ -102,26 +132,13 @@ p_2a = plot(
     title="Utility Function Interpolation Comparison"
 )
 
-function perc_true(arr)
-    sum(arr) * 100. / length(arr)
-end
-
-function check_properties(x, y)
-    y_diff = y[2:end] - y[1:(end-1)]
-    x_diff = x[2:end] - x[1:(end-1)]
-    diff_ratio = y_diff ./ x_diff
-
-    @printf "Monotonicity: %.1f%%\n" perc_true(y_diff .> 0)
-    @printf "Concavity: %.1f%%\n" perc_true(diff_ratio[1:(end-1)] .> diff_ratio[2:end])
-    @printf "Negativity: %.1f%%\n" perc_true(y .< 0)
-
-end
-
 println("2 (b)")
 check_properties(x_j, itp.(x_j))
 
+# get a polynomial grid
 z_j = a .+ (b - a)*range(0,1,length=100).^c
 
+# use this to interpolate, specify no boundaries because we need to extrapolate
 itp_poly = CubicInterpolator(z_j, u(z_j), NoBoundaries())
 
 println("3 (a)")
@@ -137,6 +154,7 @@ p_3a = plot(
 println("3 (b)")
 check_properties(x_j, itp_poly.(x_j))
 
+# get the right extrapolation range
 x_30_40 = range(30, 40, length = 100)
 
 p_4a = plot(
@@ -150,6 +168,7 @@ println("4 (a)")
 @printf "Maximum error is %.6f\n" maximum(abs.(u(x_30_40) - itp_poly.(x_30_40)))
 check_properties(x_30_40, itp_poly.(x_30_40))
 
+# get the left extrapolation range
 x_001_01 = range(.01, .1, length = 100)
 
 p_4b = plot(
@@ -164,9 +183,6 @@ println("4 (b)")
 check_properties(x_001_01, itp_poly.(x_001_01))
 
 # save files
-
-path = "/Users/hlukas/Google Drive/Grad School/2022-2023/Spring/ECON 527/Assignments/Assignment_1/"
-
 savefig(p_2a, path * "p_2a.png")
 savefig(p_3a, path * "p_3a.png")
 savefig(p_4a, path * "p_4a.png")
